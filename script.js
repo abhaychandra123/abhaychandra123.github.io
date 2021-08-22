37b32eaf096eca1c223a8acd5db1a0ac099355e5
Chinmay <chinmaybarik214@gmail.com>
2021-08-22 13:07:50 +0530

    Added sidebar + Text

---
+++ script.js
@@ -0,0 +1,11 @@
+// js for sidebar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+// js for sidebar
